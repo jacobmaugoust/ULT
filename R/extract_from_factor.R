709cfd7e51780a799a1d94422a3829086a9fff51
Jacob.Maugoust <jacob.maugoust@umontpellier.fr>
2020-08-04 17:06:43 +0200

    extract.from.factor update

--- R/extract_from_factor.R
+++ R/extract_from_factor.R
@@ -11,26 +11,62 @@
 #' A factor vector with only the specified levels in f2
 #'
 #' @examples
-#' f1<-as.factor(c(rep("A",10),rep("B",5),rep("C",8),rep("A",4)))
-#' f2<-c("A","B")
-#' extract.from.factor(f1,f2)
+#' factor_vector<-as.factor(c(rep("A",10),rep("B",5),rep("A",4),rep("C",6)))
+#' factor_levels<-c("B","C")
+#' x<-c(rep(10,10),rep(5,5),rep(11,4),rep(9,6))
+#'
+#' extract.from.factor(factor_vector,factor_levels,output="factor")
+#' extract.from.factor(factor_vector,factor_levels,output="logical")
+#' extract.from.factor(factor_vector,factor_levels,x,output="data")
 #'
 #' @export
 
-extract.from.factor<-function(f1,f2){
-  if(is.factor(f1)==FALSE){
-    f1<-as.factor(f1)
+extract.from.factor<-function(factor_vector,factor_levels,x=NA,output){
+  if(is.factor(factor_vector)==FALSE){
+    factor_vector<-as.factor(factor_vector)
   }
-  if(is.character(f2)==FALSE){
-    f2<-as.character(f2)
+  if(is.character(factor_levels)==FALSE){
+    factor_levels<-as.character(factor_levels)
   }
+  if(missing(output)){
+    if(missing(x)){
+      output<-"factor"
+    }
+    else{
+      output<-"data"
+    }
+  }
+  if(length(x)!=length(factor_vector)){
+    errorCondition("Error: data vector does not have the same length than the factor vector")
+  }
+
   result<-c()
-  for (i in 1:length(f1)){
-    for (j in 1:length(f2)){
-      if(f1[i]==f2[j]){
-        result<-c(result,as.character(f1[i]))
+  for (i in 1:length(factor_vector)){
+    for (j in 1:length(factor_levels)){
+      if(output=="factor"){
+        if(factor_vector[i]==factor_levels[j]){
+          result<-c(result,as.character(factor_vector[i]))
+        }
+      }
+      if(output=="data"){
+        if(factor_vector[i]==factor_levels[j]){
+          result<-c(result,x[i])
+        }
+      }
+    }
+    if(output=="logical"){
+      if(any(factor_vector[i]==factor_levels)){
+        result<-c(result,TRUE)
+      }
+      else{
+        result<-c(result,FALSE)
       }
     }
   }
-  return(as.factor(result))
+  if(output=="factor"){
+    return(as.factor(result))
+  }
+  else{
+    return(result)
+  }
 }
