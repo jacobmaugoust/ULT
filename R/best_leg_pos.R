a2b994e7807fbafee2bed329eedced78ba5ab3f1
jacobmaugoust <jacob.maugoust@umontpellier.fr>
2022-09-13 15:07:06 +0200

    Added internal function best.leg.pos

---
+++ R/best_leg_pos.R
@@ -0,0 +1,21 @@
+#' @title Find best legend position in a plot
+#' @description Quick function to automatically add a legend in the plot quarter which contains the less points
+#' @param x The x data or the xy dataset
+#' @param y Optional. The y data
+#' @export
+#' @keywords internal
+
+best.leg.pos<-function(x,y){
+  if(missing(y)&ncol(x)==2){
+    y<-x[,2]
+    x<-x[,1]
+  }
+  xlim<-par("usr")[1:2]
+  ylim<-par("usr")[3:4]
+  res<-c("topleft","topright","bottomleft","bottomright")[which.min(c(
+    length(which(x<mean(xlim)&y>mean(ylim))),
+    length(which(x>mean(xlim)&y>mean(ylim))),
+    length(which(x<mean(xlim)&y<mean(ylim))),
+    length(which(x>mean(xlim)&y<mean(ylim)))))]
+  return(res)
+}
