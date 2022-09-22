4c562996f8847cab00486b603e347a95ca3a3caf
jacobmaugoust <jacob.maugoust@umontpellier.fr>
2022-09-22 20:36:36 +0200

    Fixed add.ages.phylo function for taking into account tip and/or node ages

--- R/add_ages_phylo.R
+++ R/add_ages_phylo.R
@@ -12,6 +12,7 @@
 #'
 #' @examples
 #' require(ape)
+#' ### Define all tip and node ages
 #' # Create an ultrametric tree
 #' tree<-rtree(20)
 #' tree<-tree[-which(names(tree)=="edge.length")]
@@ -22,6 +23,26 @@
 #' node.ages<-sort(runif(Nnode(tree),6,10),decreasing = TRUE)
 #' # Add tip and node ages to the tree
 #' newtree<-add.ages.phylo(tree,tip.ages,node.ages)
+#'
+#' ### Change some tip and node ages
+#' # Create a tree
+#' set.seed(1)
+#' tree<-rtree(20)
+#' par(mfrow=c(1,3),mar=c(1,0,0,0),mgp=c(0,0,0))
+#' plot(tree)
+#' axisPhylo()
+#' # Modify some tips
+#' tip.ages<-(max(nodeHeights(tree))-nodeHeights(tree)[tree$edge[,2]<=Ntip(tree),2])
+#' set.seed(3)
+#' tips_to_replace<-sample(c(1:Ntip(tree)),runif(1,1,Ntip(tree)),replace=FALSE)
+#' tip.ages[tips_to_replace]<-runif(length(tips_to_replace),min((max(nodeHeights(tree))-nodeHeights(tree))[tree$edge[,2]<=Ntip(tree),2]),min((max(nodeHeights(tree))-nodeHeights(tree))[tree$edge[,2]>Ntip(tree),2]))
+#' newTree<-add.ages.phylo(tree,tip.ages)
+#' tiplabels(rep("here",length(tips_to_replace)),tips_to_replace)
+#' # Modify some nodes
+#' node.ages<-c(max(nodeHeights(tree)),(max(nodeHeights(tree))-nodeHeights(tree)[tree$edge[,2]>Ntip(tree),2]))
+#' node.ages[c(2,8,18)]<-c(4,2.1,1.05)
+#' newTree<-add.ages.phylo(tree,node.ages=node.ages)
+#' nodelabels(rep("here",3),node=c(2,8,18)+Ntip(tree))
 #'
 #' @export
 
@@ -40,7 +61,7 @@ add.ages.phylo<-function(tree,tip.ages=NULL,node.ages=NULL,plot=TRUE){
 
   if(!is.null(node.ages)){
     if(all(is.null(names(node.ages)))){
-      names(node.ages)<-tree$node.label
+      names(node.ages)<-rev(tree$node.label)
     }
   }
 
@@ -75,20 +96,23 @@ add.ages.phylo<-function(tree,tip.ages=NULL,node.ages=NULL,plot=TRUE){
   names(nodes)<-tree$node.label
   nodes<-lapply(nodes,function(x){rev(x)})
 
-  NT_arguments<-list(taxa=tree$tip.label,
-                     nodes=nodes,
-                     plot=plot)
-  if(!is.null(tip.ages)){
-    NT_arguments<-c(NT_arguments,list(age_taxa=tip.ages[match(tree$tip.label,names(tip.ages))]))
+  NT_arguments<-list("taxa"=tree$tip.label,
+                     "nodes"=nodes,
+                     "plot"=plot)
+  if(is.null(tip.ages)){
+    tip.ages<-setNames((max(nodeHeights(tree))-nodeHeights(tree)[,2])[tree$edge[,2]<=Ntip(tree)],tree$tip.label)
   }
-  if(!is.null(node.ages)){
-    NT_arguments<-c(NT_arguments,list(age_nodes=node.ages[match(tree$node.label,names(node.ages))]))
+  if(is.null(node.ages)){
+    node.ages<-setNames(c(max(nodeHeights(tree)),(max(nodeHeights(tree))-nodeHeights(tree)[,2])[tree$edge[,2]>Ntip(tree)]),rev(tree$node.label))
   }
+  NT_arguments<-c(NT_arguments,
+                  list("age_taxa"=tip.ages[match(tree$tip.label,names(tip.ages))]),
+                  list("age_nodes"=node.ages[match(tree$node.label,names(node.ages))]))
   if(is.node.label){
-    NT_arguments<-c(NT_arguments,node.labels=tree$node.label)
+    NT_arguments<-c(NT_arguments,"node.labels"=tree$node.label)
   }
   else{
-    NT_arguments<-c(NT_arguments,node.labels=FALSE)
+    NT_arguments<-c(NT_arguments,"node.labels"=FALSE)
   }
   new.tree<-do.call("create.tree",NT_arguments)
   if(plot){
@@ -96,4 +120,3 @@ add.ages.phylo<-function(tree,tip.ages=NULL,node.ages=NULL,plot=TRUE){
   }
   return(new.tree)
 }
-
